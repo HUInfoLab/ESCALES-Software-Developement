15e159a230873010931cab5a55264af56fb40c61
tjm5114 <tjm5114@gmail.com>
2015-02-11 14:56:26 -0500

    Added CSV writer functionality

--- SoupParser.py
+++ SoupParser.py
@@ -1,7 +1,11 @@
 from bs4 import BeautifulSoup
-
+import csv
 import requests
 
+with open('SoupData.csv', 'wb') as csvfile:
+		soupwriter = csv.writer(csvfile, delimiter=',',
+								quotechar= '|', quoting=csv.QUOTE_MINIMAL)
+		soupwriter.writerow('Hello World')
 url = raw_input("Enter a website to extract the URL's from: ")
 
 r  = requests.get("http://" +url)
@@ -12,4 +16,6 @@ soup = BeautifulSoup(data)
 
 for link in soup.find_all('a'):
     print(link.get('href'))
+	
+	
 raw_input("Are you done yet?: ")
